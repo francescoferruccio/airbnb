33557959876db47edd3263ff6a6fef6bd6b54a0d
Cristiano <valentisenior@gmail.com>
2020-07-04 16:22:22 +0200

    card spons

--- team1finaltest/resources/views/content.blade.php
+++ team1finaltest/resources/views/content.blade.php
@@ -2,8 +2,27 @@
 
 @section("content")
 
+
   <div class="content">
-    <p>Contenuto</p>
+    <div class="cardRent"onclick="window.location='{{route('create')}}'">
+      <div class="cardimg">
+          <img src="images/carduno.jpg" alt="fdkcrw">
+      </div>
+      <div class="cardtext">
+        <h1>Nome Residenza</h1>
+        <p>Vieni a trascorrere una meravigliosa vacanza nella nostra fantasmagorica dimora</p>
+      </div>
+    </div>
+    <div class="cardRent"onclick="window.location='http://google.com';">
+      <div class="cardimg">
+          <img src="images/carduno.jpg" alt="fdkcrw">
+      </div>
+      <div class="cardtext">
+        <h1>Nome Residenza</h1>
+        <p>Vieni a trascorrere una meravigliosa vacanza nella nostra fantasmagorica dimora</p>
+      </div>
+    </div>
+
   </div>
 
 
