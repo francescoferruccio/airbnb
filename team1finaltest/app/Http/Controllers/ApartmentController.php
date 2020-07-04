f65c1ad80e9bd1f9a7851be67df22ebe6923a38e
Danilo <danilo.maida95@libero.it>
2020-07-04 15:34:06 +0200

    Image validator and services

--- team1finaltest/app/Http/Controllers/ApartmentController.php
+++ team1finaltest/app/Http/Controllers/ApartmentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\User;
 use App\Apartment;
+use App\Service;
+use Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -10,8 +12,9 @@ class ApartmentController extends Controller
 {
     public function create(){
 
+      $services = Service::all();
       $user = Auth::user();
-      return view('createApartment', compact('user'));
+      return view('createApartment', compact('user','services'));
     }
 
     public function store(Request $request, $id){
@@ -22,18 +25,20 @@ class ApartmentController extends Controller
       $apartment = new Apartment;
 
       $validatedData = $request -> validate([
-        "name" => "required|alpha",
-        "description" => "required|alpha-num",
+        "name" => "required|string",
+        "description" => "required|string",
         "rooms" => "required|integer",
         "beds" => "required|integer",
         "bathrooms" => "required|integer",
         "size" => "required|numeric",
-        "address" => "required|alpha-num",
+        "address" => "required|string",
         // "latitude" => "required|numeric",
         // "longitude" => "required|numeric",
-        "picture" => "nullable"
-        // "show" => "required|boolean"
+        "picture" => "required|image|mimes:jpeg,bmp,png,jpg|max:5000",
+        "services" => "nullable|array"
       ]);
+
+
 
       $apartment -> name = $validatedData['name'];
       $apartment -> description = $validatedData['description'];
@@ -44,13 +49,30 @@ class ApartmentController extends Controller
       $apartment -> address = $validatedData['address'];
       $apartment -> latitude = 23.764788;
       $apartment -> longitude =23.764788;
-      $apartment -> picture = $validatedData['picture'];
       $apartment -> show = 1;
       $apartment -> user_id = $user -> id;
 
+      //VALIDAZIONE DELL'IMAGE CARICATA LATO CLIENT
+      $image = $request->file('picture');
+      $name = Str::slug($request->input('name')).'_'.time();
+      $ext = $image->getClientOriginalExtension();
+      $folder = '/uploads/images/';
+      $filePath = $folder . $name. '.' . $ext;
+
+      $apartment -> picture = $filePath;
+
+      $image->storeAs($folder, $name.'.'.$ext, "public");
+
       $apartment -> save();
 
-      return redirect() -> route('create');
+      // VALIDAZIONE DEI SERVIZI E RELAZIONARLI ALL'APPARTAMENTO
+        if (!array_key_exists('services',$validatedData)) {
+          $apartment -> services() -> sync([]);
+        }else {
+          $apartment -> services() -> sync($validatedData['services']);
+        }
 
+      return redirect() -> route('create')
+                        -> with("status","Appartamento aggiunto con successo");
     }
 }
--- team1finaltest/resources/views/createApartment.blade.php
+++ team1finaltest/resources/views/createApartment.blade.php
@@ -1,140 +1,177 @@
 @extends('layouts.app')
 
+
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Inserisci il tuo appartamento') }}</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ __('Inserisci il tuo appartamento') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('store', $user -> id) }}">
-                        @csrf
-                        @method('POST')
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+          {{-- MESSAGGIO DI SUCCESSO DA SPOSTARE NELLA APARTMENTSHOW  --}}
+          @if (session('status'))
+              <h1>{{ session('status') }}</h1>
+          @endif
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+          <div class="card-body">
+            <form method="POST" action="{{ route('store', $user -> id) }}" enctype="multipart/form-data" role="form">
+              @csrf
+              @method('POST')
+              <div class="form-group row">
+                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                <div class="col-md-6">
+                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+
+                    @error('name')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}</label>
+
+                  <div class="col-md-6">
+                    <textarea id="description" rows="4" cols="50" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" autocomplete="description" placeholder="Inserisci la descrizione dell'appartamento..." autofocus> </textarea>
+
+                      @error('description')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="rooms" class="col-md-4 col-form-label text-md-right">{{ __('Stanze') }}</label>
+
+                    <div class="col-md-6">
+                      <input id="rooms" min="1" type="number" class="form-control @error('rooms') is-invalid @enderror" name="rooms" value="{{ old('rooms') }}" autocomplete="rooms" placeholder="1" autofocus>
+
+                        @error('rooms')
+                          <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                          </span>
+                        @enderror
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="beds" class="col-md-4 col-form-label text-md-right">{{ __('Letti') }}</label>
+
+                      <div class="col-md-6">
+                        <input id="beds" min="1" type="number" class="form-control @error('beds') is-invalid @enderror" name="beds" value="{{ old('beds') }}" autocomplete="beds" placeholder="1" autofocus>
+
+                          @error('beds')
+                            <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                        </div>
+                      </div>
+
+                      <div class="form-group row">
+                        <label for="bathrooms" class="col-md-4 col-form-label text-md-right">{{ __('Bagni') }}</label>
+
+                        <div class="col-md-6">
+                          <input id="bathrooms" min="1" type="number" class="form-control @error('bathrooms') is-invalid @enderror" name="bathrooms" value="{{ old('bathrooms') }}" autocomplete="bathrooms" placeholder="1" autofocus>
+
+                            @error('bathrooms')
+                              <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                              </span>
+                            @enderror
+                          </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}</label>
+                          <label for="size" class="col-md-4 col-form-label text-md-right">{{ __('Superficie(mq)') }}</label>
 
-                            <div class="col-md-6">
-                                <textarea id="description" rows="4" cols="50" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" autocomplete="description" placeholder="Inserisci la descrizione dell'appartamento..." autofocus> </textarea>
+                          <div class="col-md-6">
+                            <input id="size" min="1" type="number" class="form-control @error('size') is-invalid @enderror" name="size" value="{{ old('size') }}" autocomplete="size" autofocus>
 
-                                @error('description')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                              @error('size')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="rooms" class="col-md-4 col-form-label text-md-right">{{ __('Stanze') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="rooms" min="1" type="number" class="form-control @error('rooms') is-invalid @enderror" name="rooms" value="{{ old('rooms') }}" autocomplete="rooms" placeholder="1" autofocus>
-
-                                @error('rooms')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="beds" class="col-md-4 col-form-label text-md-right">{{ __('Letti') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="beds" min="1" type="number" class="form-control @error('beds') is-invalid @enderror" name="beds" value="{{ old('beds') }}" autocomplete="beds" placeholder="1" autofocus>
-
-                                @error('beds')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="bathrooms" class="col-md-4 col-form-label text-md-right">{{ __('Bagni') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="bathrooms" min="1" type="number" class="form-control @error('bathrooms') is-invalid @enderror" name="bathrooms" value="{{ old('bathrooms') }}" autocomplete="bathrooms" placeholder="1" autofocus>
-
-                                @error('bathrooms')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="size" class="col-md-4 col-form-label text-md-right">{{ __('Superficie(mq)') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="size" min="1" type="number" class="form-control @error('size') is-invalid @enderror" name="size" value="{{ old('size') }}" autocomplete="size" autofocus>
-
-                                @error('size')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                          </div>
 
 
-                        <div class="form-group row">
+                          <div class="form-group row">
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo') }}</label>
 
                             <div class="col-md-6">
-                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
+                              <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
 
                                 @error('address')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                  <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                  </span>
                                 @enderror
+                              </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="picture" class="col-md-4 col-form-label text-md-right">{{ __('Immagine') }}</label>
+                            <div class="form-group row">
+                              <label for="picture" class="col-md-4 col-form-label text-md-right">{{ __('Immagine') }}</label>
 
-                            <div class="col-md-6">
+                              <div class="col-md-6">
+                                <input type="hidden" name="MAX_FILE_SIZE" value="50971520" />
                                 <input id="picture" type="file" accept="image/x-png,image/gif,image/jpeg" class="form-control @error('picture') is-invalid @enderror" name="picture" required autocomplete="new-picture">
 
-                                @error('picture')
+                                  @error('picture')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                      <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                  @enderror
+                                </div>
+                              </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Inserisci') }}
-                                </button>
+                              <div class="form-group row">
+                                <div class="col-md-12">
+
+                                  <label for="services[]" class="col-md-4 col-form-label text-md-right">{{ __('Servizi:') }}</label>
+                                </div>
+
+                                @foreach ($services as $service)
+                                  <div class="col-md-10">
+                                    <div class="d-flex justify-content-center">
+
+                                      <div class="col-md-4">
+
+                                        <input id="service" type="checkbox" @error('services[]') is-invalid @enderror name="services[]" autocomplete="new-service_id" value ="{{$service['id']}}">
+                                          {{$service -> name}}
+                                        </div>
+                                      </div>
+
+                                      @error('services[]')
+                                        <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                        </span>
+                                      @enderror
+
+
+                                    </div>
+                                  @endforeach
+                                </div>
+
+
+
+                                <div class="form-group row mb-0">
+                                  <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                      {{ __('Inserisci') }}
+                                    </button>
+                                  </div>
+                                </div>
+                              </form>
                             </div>
+                          </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                      </div>
+                    </div>
+                  @endsection
