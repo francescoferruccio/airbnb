242036ec62356f75de5fdd6fed88a0135200d291
Danilo <danilo.maida95@libero.it>
2020-07-06 19:49:30 +0200

    Revert "Sizes fixed CSS: Header/nav"
    This reverts commit 420b32b5a63062f7447a6b14e0854f0f4289a321.

--- team1finaltest/resources/views/nav.blade.php
+++ team1finaltest/resources/views/nav.blade.php
@@ -33,11 +33,6 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                          <a class="dropdown-item" href="{{ route('user') }}">
-                              {{ __('Il mio profilo') }}
-                          </a>
-                          
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
