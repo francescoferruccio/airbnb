2fabffbc3ed96a0c0eac7b50eaa2792588beb747
Francesco <francesco.ferruccio88@gmail.com>
2020-07-07 12:59:19 +0200

    fix hover slider su firefox

--- team1finaltest/public/js/app.js
+++ team1finaltest/public/js/app.js
@@ -60308,8 +60308,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Dani\Desktop\Boolean Career\boolbnb\team1finaltest\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Dani\Desktop\Boolean Career\boolbnb\team1finaltest\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Boolean\esercizi\repo-github\airbnb\team1finaltest\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Boolean\esercizi\repo-github\airbnb\team1finaltest\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
