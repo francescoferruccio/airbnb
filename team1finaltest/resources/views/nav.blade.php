f64bd92589aa173e99d7ff6ab91257babc9d6939
Danilo <danilo.maida95@libero.it>
2020-07-13 19:13:48 +0200

    logo airbnb / nav / containers

--- team1finaltest/resources/views/nav.blade.php
+++ team1finaltest/resources/views/nav.blade.php
@@ -1,8 +1,9 @@
 {{-- navbar con register e login --}}
-<nav class="navbar fixed-top navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
+<nav id="customNav" class="navbar fixed-top navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container-fluid" id="customContainer">
         <a class="navbar-brand" href="{{ url('/') }}">
-            BoolBnB
+          <img src="/images/logobnb.png" alt="logo bnb">
+          <span class="logoTitle">BoolBnB</span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
