4a34027965b9fb91969d260c4d9686ba3de1d8c8
simone <sasso.96@libero.it>
2020-07-14 18:52:04 +0200

    messaggi

--- team1finaltest/resources/views/inbox.blade.php
+++ team1finaltest/resources/views/inbox.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="inbox">
+  {{-- <div class="inbox">
     <table>
       <thead>
         <tr>
@@ -26,6 +26,45 @@
         @endforeach
       </tbody>
     </table>
+  </div> --}}
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class='userHeader'>
+            <span>{{ __('Messaggi Ricevuti') }}</span>
+          </div>
+          <div class="msgcontainer">
+            <div class="msgsubcontainer">
+              @foreach ($allMessages as $message)
+                <div class="msgcard">
+                  <div><b>Email mittente:</b> {{$message["email"]}}</div>
+                  <div><b>Messaggio:</b> {{$message["message"]}}</div>
+
+
+                  @foreach ($apartments as $apartment)
+                    @if ($apartment['id'] == $message['apartment_id'])
+                      <div>
+                        <b>Nome appartamento:</b> <a href="{{ route('show', $apartment['id']) }}">{{ $apartment['name'] }}</a>
+
+                      </div>
+                    @endif
+                  @endforeach
+
+                </div>
+
+              @endforeach
+
+            </div>
+
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+
   </div>
 
 @endsection
