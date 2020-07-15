6000104ec72c09fc96afb7852c9a6c0480513d79
Cristiano <valentisenior@gmail.com>
2020-07-15 13:24:32 +0200

    search appart non spons

--- team1finaltest/resources/views/search.blade.php
+++ team1finaltest/resources/views/search.blade.php
@@ -6,7 +6,6 @@
     @if (count($sponsored_apts))
       <h1>Appartamenti in evidenza</h1>
       @foreach ($sponsored_apts as $sponsored)
-        <h2>ID: {{ $sponsored['id'] }}</h2>
         <h2>NAME: {{ $sponsored['name'] }}</h2>
         <ul>
           @foreach ($sponsored -> services as $service)
@@ -28,15 +27,44 @@
     @foreach ($notSponsored_apts as $notSponsored)
 
       <div class="appartamententiNormali">
-        <img src="{{$notSponsored['picture']}}" alt="">
-        <h2>ID: {{ $notSponsored['id'] }}</h2>
-        <h2>NAME: {{ $notSponsored['name'] }}</h2>
-        <ul>
-          @foreach ($notSponsored -> services as $service)
-            <li>{{ $service['name'] }}</li>
-          @endforeach
-        </ul>
+        <div class="imgApartment">
+          <img src="{{$notSponsored['picture']}}" alt="">
+        </div>
+        {{-- <h2>ID: {{ $notSponsored['id'] }}</h2> --}}
+        <div class="textApartment">
+          <div class="titleApartment">
+            <h2>{{ $notSponsored['name'] }}</h2>
+            <h4>{{ $notSponsored['address'] }}</h4>
+          </div>
+          <div class="descriptionApartment">
+            <p>{{ $notSponsored['description'] }}</p>
+          </div>
 
+        </div>
+        <div class="serviceApartment">
+          <div class="specifiche">
+            <ul class="iconList">
+              <li><i class="fas fa-toilet"></i>  {{ $notSponsored['rooms'] }}</li>
+              <li><i class="fas fa-bed"></i>  {{ $notSponsored['beds'] }}</li>
+              <li><i class="fas fa-toilet"></i>  {{ $notSponsored['bathrooms'] }}</li>
+              <li><i class="fas fa-square"></i>  {{ $notSponsored['size'] }}</li>
+            </ul>
+          </div>
+          <div class="service">
+
+            <ul>
+              @foreach ($notSponsored -> services as $service)
+                  <li><img src="/images/{{$service['name']}}.svg" alt="">
+                @php
+                if (strpos($service -> name, '_')) {
+                      $service -> name = str_replace("_", " ", $service -> name);
+                    }
+                @endphp
+                  {{$service['name']}}</li>
+              @endforeach
+            </ul>
+          </div>
+      </div>
       </div>
     @endforeach
 
