ce2a626da9c2dc1ca2a9366faf999fc643975ca2
Francesco <francesco.ferruccio88@gmail.com>
2020-07-20 14:08:13 +0200

    aggiunta grafica pagina reset password

--- team1finaltest/resources/views/auth/passwords/email.blade.php
+++ team1finaltest/resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="loginHeader"><span>Resetta Password</span></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,10 +18,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo E-Mail</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="stileForm @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -33,8 +33,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                <button type="submit" class="bottoneSubmitLogin">
+                                  Conferma
                                 </button>
                             </div>
                         </div>
