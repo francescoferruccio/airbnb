1a0174b069f8df2edb821b62d8e93377637579d9
simone <sasso.96@libero.it>
2020-07-20 12:31:52 +0200

    footer

--- team1finaltest/resources/views/footer.blade.php
+++ team1finaltest/resources/views/footer.blade.php
@@ -1,27 +1,27 @@
 <div class="footer">
 <ul>
   <li> <h5>Informazioni</h5> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
+  <li><a href="#">Come funziona Airbnb</a></li>
+  <li><a href="#">Airbnb Magazine</a></li>
+  <li><a href="#">Airbnb Citizen</a></li>
+  <li><a href="#">Diversità e appartenenza</a></li>
+  <li><a href="#">Accessibilità</a></li>
 </ul>
 <ul>
   <li> <h5>Community</h5> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
+  <li> <a href="#">Opportunità di lavoro</a> </li>
+  <li> <a href="#">Invita degli amici</a> </li>
+  <li> <a href="#">Airbnb for Work</a></li>
+  <li> <a href="#">Offri un'esperienza online</a></li>
+  <li> <a href="#">Ospitare responsabilmente</a> </li>
 </ul>
 <ul>
-  <li> <h5>Ospita</h5> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
-  <li> <a href="#">Link</a> </li>
+  <li> <h5>Realizzato da</h5> </li>
+  <li class="li-icon"><div>Cristiano Valenti</div> <div><a href="https://www.linkedin.com/in/cristiano-valenti-a66571140/"><img class="footer-icon" src="/images/linkedin.svg" alt="icon"></a> <a href="https://github.com/CristianoSenior"><img class="footer-icon" src="/images/github.svg" alt="icon"></a></div></li>
+  <li class="li-icon"><div>Danilo Maida</div> <div><a href="https://www.linkedin.com/in/danilo-maida"><img class="footer-icon" src="/images/linkedin.svg" alt="icon"></a> <a href="https://github.com/DannyM-web"><img class="footer-icon" src="/images/github.svg" alt="icon"></a></div></li>
+  <li class="li-icon"><div>Fabio Di Gennaro</div> <div><a href="https://www.linkedin.com/in/cristianovalenti/"><img class="footer-icon" src="/images/linkedin.svg" alt="icon"></a> <a href="https://github.com/Alumina14"><img class="footer-icon" src="/images/github.svg" alt="icon"></a></div></li>
+  <li class="li-icon"><div>Francesco Ferruccio</div> <div><a href="https://www.linkedin.com/in/francesco-ferruccio/"><img class="footer-icon" src="/images/linkedin.svg" alt="icon"></a> <a href="https://github.com/francescoferruccio"><img class="footer-icon" src="/images/github.svg" alt="icon"></a></div></li>
+  <li class="li-icon"><div>Simone Sasso</div> <div><a href="https://www.linkedin.com/in/simone-sasso/"><img class="footer-icon" src="/images/linkedin.svg" alt="icon"></a> <a href="https://github.com/simonesasso"><img class="footer-icon" src="/images/github.svg" alt="icon"></a></div></li>
 </ul>
 </div>
 <div class="sottoFooter">
